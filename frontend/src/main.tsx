5819275689fa5ee60518125b6c549fea4f89c831
niqzart <qwert45hi@yandex.ru>
2025-07-05 19:11:23 +0300

    feat: setup frontend to work with shadcn

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,9 +1,21 @@
+import { Tooltip, TooltipContent, TooltipTrigger } from "@/shared/ui/tooltip"
 import { StrictMode } from "react"
 import { createRoot } from "react-dom/client"
 import "./index.css"
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <h1>Delta Sync</h1>
+    <Tooltip>
+      <TooltipTrigger asChild>
+        <h1 className="scroll-m-20 text-center text-4xl font-extrabold tracking-tight text-balance">
+          Delta Sync
+        </h1>
+      </TooltipTrigger>
+      <TooltipContent>
+        <p className="w-80">
+          Delta-based synchronization demo web application made with FastAPI, SocketIO (TMEXIO), SQLAlchemy, React & Vite
+        </p>
+      </TooltipContent>
+    </Tooltip>
   </StrictMode>,
 )
